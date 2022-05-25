6262cf7c7800dbfdfc650392b7841d671d942e84
whos-bax <loot04@naver.com>
2022-05-25 09:32:03 +0900

    정리

--- whoami/frontend/vue.config.js
+++ whoami/frontend/vue.config.js
@@ -2,15 +2,3 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-module.exports = {
-  css: {
-    loaderOptions: {
-      scss: {
-        prependData: `
-          @import "@/assets/scss/variables.scss";
-        `,
-      },
-    },
-  },
-
-};
