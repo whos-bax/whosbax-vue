f0e829c41b46f8fcfef3a2a731d1969120536662
whos-bax <loot04@naver.com>
2022-05-27 13:29:55 +0900

    scss

--- whoami/frontend/src/App.vue
+++ whoami/frontend/src/App.vue
@@ -21,9 +21,9 @@ export default {
 </script>
 
 <style lang="scss">
-@import "@/assets/scss/main.scss";
-#test {
-  background-color: $TEST;
+
+#app {
+  background-color: #555;
 }
 
 </style>
--- whoami/frontend/src/components/MainNav.vue
+++ whoami/frontend/src/components/MainNav.vue
@@ -20,8 +20,5 @@ export default {
 </script>
 
 <style lang='scss'>
-.mainNav {
-  background-color: $TEST_BG;
-  @include center;
-}
+
 </style>
